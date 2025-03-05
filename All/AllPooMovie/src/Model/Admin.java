5deb4f80616624a140d9b09f9c5d4095efc082c1
fahd djedi <djedifahd0@gmail.com>
2025-03-05 11:15:24 +0100

    Add new manager classes and refactor existing models for improved structure

--- All/AllPooMovie/src/Model/Admin.java
+++ All/AllPooMovie/src/Model/Admin.java
@@ -1,27 +1,5 @@
 package Model;
 
-
-public class Admin extends Account{
-
-    public String FirstName;
-    public String LastName;
-    public int ID;
-    public int Age;
-    public String Email;
-    public String PhoneNumber;
-
-    public Admin(String FirstName, String LastName, String email, String username ,String password, Status status) {
-
-        super(username, password, email,"Admin", status);
-        this.FirstName = FirstName;
-        this.LastName = LastName;
-    }
-
-    public void UpdateAdmin(Account admin, String newFirstName, String newLastName, int newID, int newAge) {
-        this.FirstName = newFirstName;
-        this.LastName = newLastName;
-        this.ID = newID;
-        this.Age = newAge;
-    }
+public class Admin {
 
 }
--- All/AllPooMovie/src/Model/Seats.java
+++ All/AllPooMovie/src/Model/Seats.java
@@ -1,20 +1,5 @@
 package Model;
 
 public class Seats {
-    public int seatNumber;
-    public boolean isAvailable;
-    public SeatType type;
-    public double price;
 
-    public enum SeatType {
-        REGULAR,
-        VIP;
-    }
-
-    public Seats(int seatNumber, SeatType type) {
-        this.seatNumber = seatNumber;
-        this.type = type;
-        this.isAvailable = true;
-    }
-
-} 
+}
